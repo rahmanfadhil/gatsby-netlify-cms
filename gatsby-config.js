4ad703d2919b2946b0343c5df3298d8326d0cd27
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-12-28 20:41:01 +0700

    Read blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-netlify"]
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "blog",
+        path: `${__dirname}/blog/`
+      }
+    },
+    "gatsby-transformer-remark",
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-netlify"
+  ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,24 @@
-import React from "react"
+import React from "react";
+import { graphql } from "gatsby";
 
-export default () => <div>Hello world!</div>
+export default ({ data }) => (
+  <div>
+    {JSON.stringify(data)}
+    Hello world!
+  </div>
+);
+
+export const query = graphql`
+  query {
+    markdownRemark {
+      id
+      frontmatter {
+        title
+        path
+        date
+        _PARENT
+      }
+      html
+    }
+  }
+`;
